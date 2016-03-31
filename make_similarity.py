c400fe72a6430829eb1c714d21a89f7af6d1009c
ryanglambert <ryan.g.lambert@gmail.com>
2016-03-31 10:02:50 -0700

    don't need to load tfidf to make similarity matrix

--- make_similarity.py
+++ make_similarity.py
@@ -3,7 +3,7 @@ from tfidf_pm import *
 print "################# MAKE SIMILARITY OBJECT #############"
 
 
-pubmed_tfidf = models.tfidfmodel.TfidfModel.load('./pmc_models_serialized/pubmed_tfidf')
+# pubmed_tfidf = models.tfidfmodel.TfidfModel.load('./pmc_models_serialized/pubmed_tfidf')
 
 pubmed_lsi = models.LsiModel.load('./pmc_models_serialized/pubmed_lsi')
 pubmed_corpus_lsi = models.LsiModel.load('./pmc_models_serialized/pubmed_corpus_lsi')
