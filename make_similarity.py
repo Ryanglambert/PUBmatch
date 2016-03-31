5279935eb59b1c1cbdbda609da1bdd55b5bbf5fd
ryanglambert <ryan.g.lambert@gmail.com>
2016-03-31 10:39:12 -0700

    adding prints to see where I am more explicitly

--- make_similarity.py
+++ make_similarity.py
@@ -8,8 +8,14 @@ print "################# MAKE SIMILARITY OBJECT #############"
 
 # pubmed_tfidf = models.tfidfmodel.TfidfModel.load('./pmc_models_serialized/pubmed_tfidf')
 
+print "################ LOADING LSI MODEL ###############"
 pubmed_lsi = models.LsiModel.load('./pmc_models_serialized/pubmed_lsi')
+
+print "################ LOADING LSI CORPUS ###############"
+
 pubmed_corpus_lsi = models.LsiModel.load('./pmc_models_serialized/pubmed_corpus_lsi')
+
+print "################ MAKING SIMILARITY MATRIX ################"
 
 pubmed_sim = similarities.MatrixSimilarity(pubmed_corpus_lsi, num_features=pubmed_lsi.num_topics)
 
