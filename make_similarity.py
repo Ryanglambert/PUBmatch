7bbf7580c63bde8a9a32f20551c1608738e4027b
ryanglambert <ryan.g.lambert@gmail.com>
2016-03-31 10:33:19 -0700

    similarity matrix specified with num features

--- make_similarity.py
+++ make_similarity.py
@@ -11,7 +11,7 @@ print "################# MAKE SIMILARITY OBJECT #############"
 pubmed_lsi = models.LsiModel.load('./pmc_models_serialized/pubmed_lsi')
 pubmed_corpus_lsi = models.LsiModel.load('./pmc_models_serialized/pubmed_corpus_lsi')
 
-pubmed_sim = similarities.MatrixSimilarity(pubmed_corpus_lsi, pubmed_lsi.num_topics)
+pubmed_sim = similarities.MatrixSimilarity(pubmed_corpus_lsi, num_features=pubmed_lsi.num_topics)
 
 pubmed_sim.save('./pmc_models_serialized/pubmed_sim')
 
