7bcff33fec62292f31d1161a04ef09d09cb6d28b
ryanglambert <ryan.g.lambert@gmail.com>
2016-04-04 15:39:15 -0700

    caching links that aren't known yet

--- similarity_server.py
+++ similarity_server.py
@@ -7,12 +7,14 @@ import fileinput
 from pprint import pprint 
 import logging
 import sys
+from lxml import html
 
 from tfidf_pm import PubmedCorpus
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 TITLE_DICT = {}
+pubmed_link_cache = {}
 
 #### should use MatrixSimilarity
 
@@ -25,6 +27,12 @@ with open('./pmc_data/file_list.txt', 'rb') as csv_f:
         pmid = row[2]
         pm_title = row[1]
         TITLE_DICT[file_name] = pmc_num, pmid, pm_title
+
+def try_pubmed_cache(pm_link):
+    try:
+        title = pubmed_link_cache[pm_link]
+    except KeyError:
+        return None
 
 def get_similarity_list(new_doc):
     new_doc = utils.tokenize(new_doc)
@@ -39,16 +47,32 @@ def get_pmc_number(doc_tuple):
         sim_num = doc_tuple[0]
         doc_file_name = DOCUMENT_FILE_NAMES[sim_num]
         pmc_num = ''
+        pmc_page_title = ''
         try:
             pmc_num = TITLE_DICT[doc_file_name][0]
             pmc_link = 'http://www.ncbi.nlm.nih.gov/pubmed/{}'.format(pmc_num)
+            try:
+                pmc_page_title = pubmed_link_cache[pmc_link]
+            except KeyError:
+                print "didn't get from cache"
+                t = html.parse(pmc_link)
+                pmc_page_title = t.find(".//title").text
+                pubmed_link_cache[pmc_link] = pmc_page_title
+
         except KeyError:
             return None
         if pmc_num == '':
             pmid = TITLE_DICT[doc_file_name][1]
             pmid_link = 'http://www.ncbi.nlm.nih.gov/pubmed/?term={}'.format(pmid)
-            return pmid_link
-        return pmc_link
+            try:
+                pmc_page_title = pubmed_link_cache[pmid_link]
+            except KeyError:
+                print "didn't get frmo cache"
+                t = html.parse(pmid_link)
+                pmc_page_title = t.find(".//title").text
+                pubmed_link_cache[pmid_link] = pmc_page_title
+            return pmid_link, pmc_page_title
+        return pmc_link, pmc_page_title
 
 def get_pmc_title(doc_tuple):
     if isinstance(doc_tuple, tuple):
@@ -68,7 +92,7 @@ MODEL_FOLDER = './pmc_models_from_remote/pmc_models_serialized_300f_pruneat20000
 # MODEL_FOLDER = './pmc_models_serialized_small/'
 
 pubmed_sim = similarities.MatrixSimilarity.load(os.path.join(MODEL_FOLDER, 'pubmed_sim'))
-pubmed_sim.num_best = 500
+pubmed_sim.num_best = 20
 pubmed_tfidf = models.tfidfmodel.TfidfModel.load(os.path.join(MODEL_FOLDER, 'pubmed_tfidf'))
 pubmed_lsi = models.LsiModel.load(os.path.join(MODEL_FOLDER, 'pubmed_lsi'))
 pubmed_corpus_lsi = models.LsiModel.load(os.path.join(MODEL_FOLDER, 'pubmed_corpus_lsi'))
@@ -96,7 +120,7 @@ def my_form_post():
     return render_template('pubmed_list.html', pmids=pmc_doc_sims)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
 
 
 
