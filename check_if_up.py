81503e91b7f537be574ea29358168fd70a0ff39b
ryanglambert <ryan.g.lambert@gmail.com>
2016-04-18 14:50:03 -0700

    check that pubmatch.co loads

---
+++ check_if_up.py
@@ -0,0 +1,33 @@
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
+import requests
+import schedule
+import time
+
+def is_pubmatch_up():
+    response = requests.get("http://www.pubmatch.co")
+    return response.ok
+
+def send_status():
+    msg = MIMEText("ALERT: PUBmatch DOWN")
+    msg["From"] = "status@pubmatch.co"
+    msg["To"] = "ryan.g.lambert@gmail.com"
+    msg["Subject"] = "ALERT: PUBmatch is DOWN"
+    p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+    p.communicate(msg.as_string())
+
+def status_checker():
+    if not is_pubmatch_up():
+        send_status()
+
+def main():
+    schedule.every(5).minutes.do(status_checker)
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
+
+
